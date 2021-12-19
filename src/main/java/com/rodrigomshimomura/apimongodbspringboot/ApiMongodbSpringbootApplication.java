d8139e9653bc54136620545de49e3489655f63b6
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-19 16:38:24 -0300

    Project created

---
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/ApiMongodbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigomshimomura.apimongodbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMongodbSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMongodbSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigomshimomura/apimongodbspringboot/ApiMongodbSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigomshimomura.apimongodbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMongodbSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
