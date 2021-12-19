dfa1a286526140827df028d6b58a4f701a7e3ab3
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-19 18:52:59 -0300

    Find user by id implemented

--- src/main/java/com/rodrigomshimomura/apimongodbspringboot/controllers/UserController.java
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/controllers/UserController.java
@@ -5,8 +5,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigomshimomura.apimongodbspringboot.domain.User;
@@ -28,6 +30,15 @@ public class UserController {
 
 		return ResponseEntity.ok().body(listDTO);
 
+	}
+	
+	@RequestMapping(method =  RequestMethod.GET, value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		
+		User obj = userService.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(obj));
+		
 	}
 
 }
--- src/main/java/com/rodrigomshimomura/apimongodbspringboot/services/UserService.java
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/services/UserService.java
@@ -1,23 +1,33 @@
 package com.rodrigomshimomura.apimongodbspringboot.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigomshimomura.apimongodbspringboot.domain.User;
 import com.rodrigomshimomura.apimongodbspringboot.repository.UserRepository;
+import com.rodrigomshimomura.apimongodbspringboot.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
-		
+
 		return userRepository.findAll();
+
+	}
+
+	public User findById(String id) {
+		
+		Optional<User> obj = userRepository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found on the Data Base."));
 		
 	}
-	
+
 }
