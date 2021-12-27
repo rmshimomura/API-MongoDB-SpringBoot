5c636590dd62ad71c139993a736c354bbd711b25
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-27 17:08:37 -0300

    Author data using DTO pattern

--- src/main/java/com/rodrigomshimomura/apimongodbspringboot/config/Instantiation.java
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.rodrigomshimomura.apimongodbspringboot.domain.Post;
 import com.rodrigomshimomura.apimongodbspringboot.domain.User;
+import com.rodrigomshimomura.apimongodbspringboot.dto.AuthorDTO;
 import com.rodrigomshimomura.apimongodbspringboot.repository.PostRepository;
 import com.rodrigomshimomura.apimongodbspringboot.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob)); // It's necessary to save all users in DB before creating the posts, or else id post author's id is going to be null
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
