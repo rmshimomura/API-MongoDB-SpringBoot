cbf24a126293fac15f50b2226871dd90d1f2072a
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-30 15:56:00 -0300

    Search by title using @Query

--- src/main/java/com/rodrigomshimomura/apimongodbspringboot/repository/PostRepository.java
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.rodrigomshimomura.apimongodbspringboot.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rodrigomshimomura.apimongodbspringboot.domain.Post;
@@ -10,6 +11,10 @@ import com.rodrigomshimomura.apimongodbspringboot.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	
+	@Query("{ 'title' : { $regex: ?0, $options : 'i' } }") // ?0 stands for first parameter, in this case the String text.
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
