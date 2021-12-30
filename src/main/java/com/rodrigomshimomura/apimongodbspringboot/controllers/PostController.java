7f20225aba6343d4eb77060b9e6f97bbfbc8eedc
rmshimomura <shimomurarodrigo1@gmail.com>
2021-12-30 16:07:54 -0300

    Full search using text, minDate and maxDate

--- src/main/java/com/rodrigomshimomura/apimongodbspringboot/controllers/PostController.java
+++ src/main/java/com/rodrigomshimomura/apimongodbspringboot/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.rodrigomshimomura.apimongodbspringboot.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,18 @@ public class PostController {
 
 		return ResponseEntity.ok().body(list);
 
+	}
+	
+	@GetMapping(value="/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
